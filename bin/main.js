0424964a817f605d6550b8321b452b99124ef0b2
Jonah <jonah.ahvonen@gmail.com>
2017-11-21 19:05:31 +0200

    Comment and clean code

--- bin/main.js
+++ bin/main.js
@@ -5,28 +5,27 @@ var Settings = require('settings');
 var argv = require('minimist')(process.argv.slice(2));
 var fs = require('fs');
 
-
 var config = new Settings(require('../lib/config.js'));
 
-//console.log(argv)
-
-// Default command call without arguments
-if(argv.v)
+// Print out gistr version, if -v or --version argument exists
+if(argv.v || argv.version)
 {
 	gistr.version();
 	process.exit(0);
 }
-else if(argv.h)
+else if(argv.h || argv.help) // Print out help if -h or --help argument exists
 {
 	gistr.help();
 	process.exit(0);
 }
-else if(argv._.length === 0)
+else if(argv._.length === 0) // If the whole command was only "gistr", we call some main qistr function
 {
 	gistr.main();
 }
 else
 {
+	// We just half-blindly call whichever subcommand the user wants to call
+	// by checking is gistr exports has such function for some subcommand exposed
 	var subcommand = argv._[0];
 
 	if(typeof gistr[subcommand] !== 'undefined')
@@ -35,6 +34,7 @@ else
 	}
 	else
 	{
+		// For example "gistr asdfg" would trigger this, as gistr.exports does not expose function with a name "asdfg"
 		console.log('No such command `' + subcommand + '`');
 		process.exit(0);
 	}
