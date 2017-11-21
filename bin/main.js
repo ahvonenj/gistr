07c2db49d01bd0782caa0769cde3ad91c71db27b
Jonah <jonah.ahvonen@gmail.com>
2017-11-21 16:07:01 +0200

    Reimplement command line usage
    + Implement alises for commands
    + Add "gist" as base command
    + Add basic tests

--- bin/main.js
+++ bin/main.js
@@ -8,51 +8,44 @@ var fs = require('fs');
 
 var config = new Settings(require('../lib/config.js'));
 
-console.log(argv)
+//console.log(argv)
 
 // Default command call without arguments
-if(argv._.length === 0)
+if(argv.v)
+{
+	gistr.version();
+	process.exit(0);
+}
+else if(argv.h)
+{
+	gistr.help();
+	process.exit(0);
+}
+else if(argv._.length === 0)
 {
 	gistr.main();
 }
 else
 {
-	if(argv.v)
-	{
-		gistr.version();
-		process.exit(0);
-	}
-	else if(argv.h)
-	{
-		gistr.help();
-		process.exit(0);
-	}
-
 	var subcommand = argv._[0];
 
-	switch(subcommand)
+	if(typeof gistr[subcommand] !== 'undefined')
 	{
-		case 'execdir':
-			gistr.execdir();
-			break;
-		case 'moduledir':
-			gistr.moduledir();
-			break;
-		case 'settings':
-			console.log(config);
-			break;
-		case 'create':
-		case 'add':
-			gistr.creategist(function(data, response)
-			{
-				fs.writeFile(process.cwd() + '/resp.json', JSON.stringify(data), function(err)
-				{
-					if(err) throw err;
-				});
-			});
-			break;
-		default:
-			console.log('No such command `' + subcommand + '`');
-			break;
+		gistr[subcommand](argv);
 	}
+	else
+	{
+		console.log('No such command `' + subcommand + '`');
+		process.exit(0);
+	}
+
+	/*
+		gistr.creategist(function(data, response)
+		{
+			fs.writeFile(process.cwd() + '/resp.json', JSON.stringify(data), function(err)
+			{
+				if(err) throw err;
+			});
+		});
+	*/
 }
--- spec/test.js
+++ spec/test.js
@@ -1,7 +1,33 @@
-describe("A suite", function() 
+describe("Core", function() 
 {
-	it("contains spec with an expectation", function()
+	var gistr = require('../lib/index.js');
+
+	it("should find gistr", function()
 	{
-		expect(true).toBe(true);
+		expect(gistr).not.toBeUndefined();
+	});
+});
+
+describe("gistr basic commands", function() 
+{
+	var gistr = require('../lib/index.js');
+	console.log = jasmine.createSpy("log");
+
+	it("should output usage information", function()
+	{
+		gistr.main();
+		expect(console.log).toHaveBeenCalledWith('Usage: gistr -h');
+	});
+
+	it("should output help page", function()
+	{
+		gistr.help();
+		expect(console.log).not.toBeNull('');
+	});
+
+	it("should output version", function()
+	{
+		gistr.version();
+		expect(console.log).not.toBeNull('');
 	});
 });
