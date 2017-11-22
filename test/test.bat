8a24317023a09280a27aeb3f0175c4b3d14c30cb
Jonah <jonah.ahvonen@gmail.com>
2017-11-22 14:38:12 +0200

    Test fuzzy search and rate limit detection - WORKING

--- test/test.bat
+++ test/test.bat
@@ -1,4 +1,4 @@
 call gist add a.txt -d="This is a file"
 call gist add b.txt -d="This is an another file"
 call gist add a.txt b.txt c.txt -b -d="Bundle of files"
-call gist add a.txt b.txt c.txt -d="Separate multiple files"
+::call gist add a.txt b.txt c.txt -d="Separate multiple files"
