d795bf3ca85c082c19195332419148789f9506b1
Jonah <jonah.ahvonen@gmail.com>
2017-11-22 16:37:50 +0200

    Supply minified files as well

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var jasmine = require('gulp-jasmine');
 var reporters = require('jasmine-reporters');
+var minify = require('gulp-minify');
 
 gulp.task('default', function()
 {
@@ -9,3 +10,24 @@ gulp.task('default', function()
 		reporter: new reporters.JUnitXmlReporter() 
 	}));
 })
+
+gulp.task('build', function()
+{
+	gulp.src('lib/*.js')
+	.pipe(minify({
+		ext:{
+			min:'-min.js'
+		},
+		ignoreFiles: ['*-min.js']
+	}))
+	.pipe(gulp.dest('lib/'))
+
+	gulp.src('bin/*.js')
+	.pipe(minify({
+		ext:{
+			min:'-min.js'
+		},
+		ignoreFiles: ['*-min.js']
+	}))
+	.pipe(gulp.dest('bin/'))
+});
